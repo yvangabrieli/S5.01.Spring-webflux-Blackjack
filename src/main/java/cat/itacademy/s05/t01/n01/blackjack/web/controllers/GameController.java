6370dd2fe0e2d2cfa7eb44b53cde16388e9d77c1
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-02-10 13:12:58 +0100

    fix : Update GameController to return Mono<GameResponse>

--- src/main/java/cat/itacademy/s05/t01/n01/blackjack/web/controllers/GameController.java
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack/web/controllers/GameController.java
@@ -1,8 +1,6 @@
 package cat.itacademy.s05.t01.n01.blackjack.web.controllers;
 
-
 import cat.itacademy.s05.t01.n01.blackjack.application.game.port.in.*;
-import cat.itacademy.s05.t01.n01.blackjack.domain.model.aggregates.Game;
 import cat.itacademy.s05.t01.n01.blackjack.web.dto.request.AddPlayerRequest;
 import cat.itacademy.s05.t01.n01.blackjack.web.dto.request.PlayMoveRequest;
 import cat.itacademy.s05.t01.n01.blackjack.web.dto.response.GameResponse;
@@ -19,46 +17,50 @@ import java.util.UUID;
 @RequestMapping("/games")
 @RequiredArgsConstructor
 public class GameController {
+
     private final CreateGameUseCase createGameUseCase;
-    private final GetGameUseCase getGameUseCase ;
+    private final GetGameUseCase getGameUseCase;
     private final PlayGameUseCase playGameUseCase;
     private final DeleteGameUseCase deleteGameUseCase;
     private final AddPlayerToGameUseCase addPlayerToGameUseCase;
 
     @PostMapping
-    public Mono<GameResponse> createGame(){
-        UUID gameId = createGameUseCase.createGame();
-        Game game = getGameUseCase.getGame(gameId);
-        return Mono.just(GameResponseMapper.from(game));
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<GameResponse> createGame() {
+        return createGameUseCase.createGame()
+                .flatMap(getGameUseCase::getGame)
+                .map(GameResponseMapper::from);
     }
 
     @GetMapping("/{gameId}")
-    public Mono<GameResponse> getGame(@PathVariable UUID gameId){
-        Game game = getGameUseCase.getGame(gameId);
-        return Mono.just(GameResponseMapper.from(game));
+    public Mono<GameResponse> getGame(@PathVariable UUID gameId) {
+        return getGameUseCase.getGame(gameId)
+                .map(GameResponseMapper::from);
     }
 
-    @PostMapping ("/{gameId}/players")
-    public Mono<GameResponse> addPlayer (
+    @PostMapping("/{gameId}/players")
+    public Mono<GameResponse> addPlayer(
             @PathVariable UUID gameId,
-            @RequestBody AddPlayerRequest request
-            ){
-        Game game =  addPlayerToGameUseCase.addPlayer(gameId, request.getName(), request.getInitialMoney());
-        return Mono.just(GameResponseMapper.from(game));
+            @Valid @RequestBody AddPlayerRequest request
+    ) {
+        return addPlayerToGameUseCase
+                .addPlayer(gameId, request.getName(), request.getInitialMoney())
+                .map(GameResponseMapper::from);
     }
 
     @PostMapping("/{gameId}/play")
     public Mono<GameResponse> playGame(
             @PathVariable UUID gameId,
             @Valid @RequestBody PlayMoveRequest request
-    ){
-        Game game = playGameUseCase.play(gameId, request.getPlayerId(),request.getMoveType());
-        return Mono.just(GameResponseMapper.from(game));
+    ) {
+        return playGameUseCase
+                .play(gameId, request.getPlayerId(), request.getMoveType())
+                .map(GameResponseMapper::from);
     }
 
-    @DeleteMapping ("/{gameId}")
+    @DeleteMapping("/{gameId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteGame(@PathVariable UUID gameId){
-        deleteGameUseCase.deleteGame(gameId);
+    public Mono<Void> deleteGame(@PathVariable UUID gameId) {
+        return deleteGameUseCase.deleteGame(gameId);
     }
 }
