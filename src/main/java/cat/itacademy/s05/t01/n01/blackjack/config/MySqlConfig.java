6f4740a67ffb18076e2c027414a79ec0fabc8a86
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-02-14 01:25:59 +0100

    fix : added profile

--- src/main/java/cat/itacademy/s05/t01/n01/blackjack/config/MySqlConfig.java
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack/config/MySqlConfig.java
@@ -3,6 +3,7 @@ package cat.itacademy.s05.t01.n01.blackjack.config;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
@@ -12,6 +13,7 @@ import javax.sql.DataSource;
 @EnableJpaRepositories(
         basePackages = "cat.itacademy.s05.t01.n01.blackjack.infrastructure.persistence.mysql"
 )
+@Profile("mysql")
 public class MySqlConfig {
 
     @Bean
@@ -21,7 +23,10 @@ public class MySqlConfig {
     ) {
         return builder
                 .dataSource(dataSource)
-                .packages("cat.itacademy.s05.t01.n01.blackjack.infrastructure.persistence.mysql")
+                .packages(
+                        "cat.itacademy.s05.t01.n01.blackjack.domain.model.aggregates",
+                        "cat.itacademy.s05.t01.n01.blackjack.domain.model.entity",
+                        "cat.itacademy.s05.t01.n01.blackjack.infrastructure.persistence.mysql")
                 .build();
     }
 }
